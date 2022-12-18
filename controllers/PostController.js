4ec4d18fd6346e8720a882caf9240ae3638cef17
ilyalonkin <Lonkin555@mail.ru>
2022-12-18 21:35:34 +0300

    Update PostController.js

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -92,9 +92,10 @@ export const getOne = async (req, res) => {
                         message: 'Статья не найдена'
                     });
                 }
-                const docMy = {...doc.data, createdAt: doc.createdAt.toISOString().substring(0, 10)};
+                const docData = {...doc.data};
+                // const docMy = {...doc, createdAt: doc.createdAt.toISOString().substring(0, 10)};
 
-                res.json(docMy);
+                res.json(docData);
             }
         ).populate('author');
 
